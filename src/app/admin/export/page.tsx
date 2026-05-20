eed44d68162f5288b93ccc84b5ca12a613276add
Elliot Marions <elliot.marions@postnord.com>
2026-05-20 20:38:43 +0200

    Rename Export page title to Statistik

--- src/app/admin/export/page.tsx
+++ src/app/admin/export/page.tsx
@@ -3,7 +3,7 @@ import { ExportView } from '@/components/ExportView'
 
 export default function AdminExportPage() {
   return (
-    <AdminLayout title="Export" sub="Sammanställ data för rapportering.">
+    <AdminLayout title="Statistik" sub="Sammanställ data för rapportering.">
       <ExportView />
     </AdminLayout>
   )
